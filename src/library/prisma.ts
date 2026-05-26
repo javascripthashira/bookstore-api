dbc30e401de0223cbb872e1a17431dfee32821ce
javascripthashira <akinsunmij@gmail.com>
2026-05-26 12:39:52 +0100

    fix: added pg-adpter to prisma

--- src/library/prisma.ts
+++ src/library/prisma.ts
@@ -1,4 +1,7 @@
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from '../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL as string });
 
 const globalForPrisma = globalThis as unknown as {
     prisma: PrismaClient | undefined;
@@ -6,9 +9,7 @@ const globalForPrisma = globalThis as unknown as {
 
 export const prisma =
     globalForPrisma.prisma ??
-    new PrismaClient({
-        log: ['query', 'error', 'warn'],
-    });
+    new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== 'production') {
     globalForPrisma.prisma = prisma;
